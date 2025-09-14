3ca83b47381000bf4580f5792e5e4bdb183340af
Yelena_global <Yelena@global.com>
2025-09-14 23:08:47 +0200

    ejercicios_if_scanner_java-upTo6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -17,13 +15,81 @@ public class Main {
                 System.out.println("Clima templado");
             }
 
-            sc.close();
 
         }
+        ;
+
+
+        //Ej.2: Detector de color fav:
+
+
+        //Ej.3: Radar de velocidad:
+        System.out.println("Ingresa velocidad de coche");
+        int speed = sc.nextInt();
+        sc.nextLine(); // <-- limpia el salto de línea
+
+        if (speed >= 120) {
+            System.out.println("Multa");
+        } else {
+            System.out.println("Velocidad correcta");
+        }
+
+
+        //Ej.4: Animales:
+
+        System.out.print("Escribe un animal: ");
+        String animal = sc.nextLine();
+
+        if (animal.equalsIgnoreCase("perro") || animal.equalsIgnoreCase("gato")) {
+            System.out.println("Es doméstico");
+        } else if (animal.equalsIgnoreCase("tigre") || animal.equalsIgnoreCase("león")) {
+            System.out.println("Salvaje");
+        }
+
+
+
+        //Ej.5: Entrada a un concierto:
+
+        System.out.println("Ingrese la edad");
+
+        int age = sc.nextInt();
+
+        if (age < 12 ){
+            System.out.println("No puede entrar");
+        } else if (age <= 17) {
+            System.out.println("Necesita adulto");
+            
+        } else {
+            System.out.println("Puede entrar solo");
+        }
+
+        //Ej.6: Semaforo:
+
+
+        System.out.println("Hay un semaforo de color...?");
+        String Semaforo = sc.next();
+
+        if ( Semaforo.equalsIgnoreCase("rojo")){
+            System.out.println("Detente");
+        } else if (Semaforo.equalsIgnoreCase("ambar")) {
+            System.out.println("Precaucion");
+        } else if (Semaforo.equalsIgnoreCase("verde")) {
+            System.out.println("puede avanzar");
+        }
+        sc.close ();
+
 
     }
 
+
 }
+
+
+
+
+
+
+
 
 
 
