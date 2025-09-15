cd0614083aaea9cc237108eb02e5aa29f6c78a70
a25elenapg <htmtf111@gmail.com>
2025-09-15 19:00:21 +0200

    ejercicios hechos todos

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa una temperatura en Cº");
-        if (sc.hasNextInt()) { //validamos que la entrada sea un numero
+        if (sc.hasNextInt()) { // se valida que la entrada sea un numero
             int grados = sc.nextInt();
 
 
@@ -16,114 +16,164 @@ public class Main {
             }
 
 
-        }
-        ;
 
 
-        //Ej.2: Detector de color fav:
+
+            //Ej.2: Detector de color fav:
+            System.out.println("Ingresa un color");
+            String color = sc.nextLine();
+
+            sc.nextLine();
+
+            if (color == "rojo" || color == "azul" || color == "verde") {
+                System.out.println("Es un color primario");
+            } else {
+                System.out.println("Otro color");
+            }
 
 
-        //Ej.3: Radar de velocidad:
-        System.out.println("Ingresa velocidad de coche");
-        int speed = sc.nextInt();
-        sc.nextLine(); // <-- limpia el salto de línea
 
-        if (speed >= 120) {
-            System.out.println("Multa");
-        } else {
-            System.out.println("Velocidad correcta");
-        }
+            //Ej.3: Radar de velocidad:
+            System.out.println("Ingresa velocidad de coche");
+            int speed = sc.nextInt();
+            sc.nextLine(); // <-- limpia el salto de línea
 
-
-        //Ej.4: Animales:
-
-        System.out.print("Escribe un animal: ");
-        String animal = sc.nextLine();
-
-        if (animal.equalsIgnoreCase("perro") || animal.equalsIgnoreCase("gato")) {
-            System.out.println("Es doméstico");
-        } else if (animal.equalsIgnoreCase("tigre") || animal.equalsIgnoreCase("león")) {
-            System.out.println("Salvaje");
-        }
+            if (speed >= 120) {
+                System.out.println("Multa");
+            } else {
+                System.out.println("Velocidad correcta");
+            }
 
 
-        //Ej.5: Entrada a un concierto:
 
-        System.out.println("Ingrese la edad");
+            //Ej.4: Animales:
 
-        int age = sc.nextInt();
+            System.out.print("Escribe un animal: ");
+            String animal = sc.nextLine();
 
-        if (age < 12) {
-            System.out.println("No puede entrar");
-        } else if (age <= 17) {
-            System.out.println("Necesita adulto");
-
-        } else {
-            System.out.println("Puede entrar solo");
-        }
-
-        //Ej.6: Semaforo:
+            if (animal.equalsIgnoreCase("perro") || animal.equalsIgnoreCase("gato")) {
+                System.out.println("Es doméstico");
+            } else if (animal.equalsIgnoreCase("tigre") || animal.equalsIgnoreCase("león")) {
+                System.out.println("Salvaje");
+            }
 
 
-        System.out.println("Hay un semaforo de color...?");
-        String Semaforo = sc.next();
 
-        if (Semaforo.equalsIgnoreCase("rojo")) {
-            System.out.println("Detente");
-        } else if (Semaforo.equalsIgnoreCase("ambar")) {
-            System.out.println("Precaucion");
-        } else if (Semaforo.equalsIgnoreCase("verde")) {
-            System.out.println("puede avanzar");
-        }
+            //Ej.5: Entrada a un concierto:
+
+            System.out.println("Ingrese la edad");
+
+            int age = sc.nextInt();
+
+            if (age < 12) {
+                System.out.println("No puede entrar");
+            } else if (age <= 17) {
+                System.out.println("Necesita adulto");
+
+            } else {
+                System.out.println("Puede entrar solo");
+            }
 
 
-        // Ejercicios por mi cuenta:
-        //1-Numero positivo o negativo:
-        System.out.println("Introduce un numero");
-        int num = sc.nextInt();
+            //Ej.6: Semaforo:
 
-        if (num < 0) {
-            System.out.println("El numero " + num + "es negativo");
-        } else if (num >= 0) {
-            System.out.println("El numero" + num + "es positivo o 0");
 
-        }
+            System.out.println("Hay un semaforo de color...?");
+            String Semaforo = sc.next();
 
-        //Ej. par o impar:
-        System.out.println("Introduce un numero");
-        int number = sc.nextInt();
+            if (Semaforo.equalsIgnoreCase("rojo")) {
+                System.out.println("Detente");
+            } else if (Semaforo.equalsIgnoreCase("ambar")) {
+                System.out.println("Precaucion");
+            } else if (Semaforo.equalsIgnoreCase("verde")) {
+                System.out.println("puede avanzar");
+            }
 
-        if (number % 2 == 0) {
-            System.out.println("El numero es par");
-        } else {
-            System.out.println("El numero es impar");
 
+
+
+            //Ej.7: Juego de adivinanza:
+            System.out.println("Ingresa un numero");
+            int number_secret = 24;
+            int suposicion;
+
+
+            System.out.println("El numero es de 1 al 30");
+            suposicion = sc.nextInt();
+
+            if (suposicion < number_secret) {
+                System.out.println("El numero es menor, try again");
+            } else if (suposicion > number_secret) {
+                System.out.println("Ahora esta por encima, prueba otra vez");
+
+            } else {
+                System.out.println("Acertó! Well done!");
+            }
+
+
+
+            //Ej.8: Comparador de palabras:
+            sc.nextLine();
+            System.out.println("Ingresa 1 palabra");
+            String palabra1 = sc.nextLine();
+
+
+          System.out.println("Ingresa la otra palabra");
+            String palabra2 = sc.nextLine();
+
+
+            if (palabra1.equalsIgnoreCase(palabra2)) {
+                System.out.println("Conciden");
+            } else {
+                System.out.println("No coinciden :( ");
+            }
+
+
+            //Ej.10: Calculadora basica:
+            System.out.println("Pon 1 numero: ");
+            double num1 = sc.nextDouble();
+            sc.nextLine();
+            System.out.println("Pon el otro numero:  ");
+            double num2 = sc.nextDouble();
+            sc.nextLine();
+
+            System.out.println("Escribe la operacion a realizar: +, - , / , *  ");
+
+            String operacion = sc.nextLine();
+
+
+            double resultado;
+
+
+            switch (operacion) {
+                case "+":
+                    resultado = num1 + num2;
+                    System.out.println("Resultado: " + resultado);
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    System.out.println("Resultado: " + resultado);
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    System.out.println("Resultado: " + resultado);
+                    break;
+                case "/":
+                    if (num2 != 0) {
+                        resultado = num1 / num2;
+                        System.out.println("Resultado: " + resultado);
+                    } else {
+                        System.out.println("Error: no se puede dividir entre 0");
+                    }
+                    break;
+                //default:
+                    //System.out.println("Operación no válida.");
+
+            }
+
+            sc.close();
         }
     }
-
-        //Ej.aprobado o suspenso:
-
-        double nota = -1;
-
-        System.out.print("Introduce la nota: ");
-        String entrada = sc.nextLine();
-        entrada = entrada.replace("," , ".");
-
-
-         nota = sc.nextDouble();
-
-        if (nota >= 5){
-            System.out.println("Ha aprobado");
-        } else {
-            System.out.println("suspenso");
-        }
-
-        sc.close ();
-
-
-    }
-
-
 }
 
 
